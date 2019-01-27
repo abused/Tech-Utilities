349659c7bd01154f88fcd0bd6a49ee9bbc44bf3f
abused_master <techperson71@gmail.com>
2019-01-26 22:14:44 -0500

    Recipes and more texture revamps!

--- src/main/java/abused_master/techutilities/registry/ModRecipes.java
+++ src/main/java/abused_master/techutilities/registry/ModRecipes.java
@@ -9,11 +9,21 @@ import net.minecraft.recipe.Ingredient;
 public class ModRecipes {
 
     public static void registerRecipes() {
+        //Furnace
         RecipeGenerator.createFurnace(new ItemStack(EnumResourceItems.COPPER_INGOT.getItemIngot()), null, Ingredient.ofStacks(new ItemStack(BlockResources.EnumResourceOres.COPPER_ORE.getBlockOres())), 1, 200);
         RecipeGenerator.createFurnace(new ItemStack(EnumResourceItems.TIN_INGOT.getItemIngot()), null, Ingredient.ofStacks(new ItemStack(BlockResources.EnumResourceOres.TIN_ORE.getBlockOres())), 1, 200);
         RecipeGenerator.createFurnace(new ItemStack(EnumResourceItems.LEAD_INGOT.getItemIngot()), null, Ingredient.ofStacks(new ItemStack(BlockResources.EnumResourceOres.LEAD_ORE.getBlockOres())), 1, 200);
         RecipeGenerator.createFurnace(new ItemStack(EnumResourceItems.SILVER_INGOT.getItemIngot()), null, Ingredient.ofStacks(new ItemStack(BlockResources.EnumResourceOres.SILVER_ORE.getBlockOres())), 1, 200);
         RecipeGenerator.createFurnace(new ItemStack(EnumResourceItems.NICKEL_INGOT.getItemIngot()), null, Ingredient.ofStacks(new ItemStack(BlockResources.EnumResourceOres.NICKEL_ORE.getBlockOres())), 1, 200);
         RecipeGenerator.createFurnace(new ItemStack(EnumResourceItems.PLATINUM_INGOT.getItemIngot()), null, Ingredient.ofStacks(new ItemStack(BlockResources.EnumResourceOres.PLATINUM_ORE.getBlockOres())), 1, 200);
+
+        RecipeGenerator.createShaped(new ItemStack(BlockResources.EnumResourceBlocks.COPPER_BLOCK.getBlockOres()), null, new RecipeGenerator.ShapedParser("XXX", "XXX", "XXX", 'X', new ItemStack(EnumResourceItems.COPPER_INGOT.getItemIngot())));
+        RecipeGenerator.createShaped(new ItemStack(BlockResources.EnumResourceBlocks.TIN_BLOCK.getBlockOres()), null, new RecipeGenerator.ShapedParser("XXX", "XXX", "XXX", 'X', new ItemStack(EnumResourceItems.TIN_INGOT.getItemIngot())));
+        RecipeGenerator.createShaped(new ItemStack(BlockResources.EnumResourceBlocks.LEAD_BLOCK.getBlockOres()), null, new RecipeGenerator.ShapedParser("XXX", "XXX", "XXX", 'X', new ItemStack(EnumResourceItems.LEAD_INGOT.getItemIngot())));
+        RecipeGenerator.createShaped(new ItemStack(BlockResources.EnumResourceBlocks.SILVER_BLOCK.getBlockOres()), null, new RecipeGenerator.ShapedParser("XXX", "XXX", "XXX", 'X', new ItemStack(EnumResourceItems.SILVER_INGOT.getItemIngot())));
+        RecipeGenerator.createShaped(new ItemStack(BlockResources.EnumResourceBlocks.NICKEL_BLOCK.getBlockOres()), null, new RecipeGenerator.ShapedParser("XXX", "XXX", "XXX", 'X', new ItemStack(EnumResourceItems.NICKEL_INGOT.getItemIngot())));
+        RecipeGenerator.createShaped(new ItemStack(BlockResources.EnumResourceBlocks.PLATINUM_BLOCK.getBlockOres()), null, new RecipeGenerator.ShapedParser("XXX", "XXX", "XXX", 'X', new ItemStack(EnumResourceItems.PLATINUM_INGOT.getItemIngot())));
+        RecipeGenerator.createShaped(new ItemStack(BlockResources.EnumResourceBlocks.INVAR_BLOCK.getBlockOres()), null, new RecipeGenerator.ShapedParser("XXX", "XXX", "XXX", 'X', new ItemStack(EnumResourceItems.INVAR_INGOT.getItemIngot())));
+        RecipeGenerator.createShaped(new ItemStack(BlockResources.EnumResourceBlocks.ELECTRUM_BLOCK.getBlockOres()), null, new RecipeGenerator.ShapedParser("XXX", "XXX", "XXX", 'X', new ItemStack(EnumResourceItems.ELECTRUM_INGOT.getItemIngot())));
     }
 }
