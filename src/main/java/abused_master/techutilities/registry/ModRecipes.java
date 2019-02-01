3dcd7fdf650a887be21e39f0d1a0860e0eb3540d
abused_master <techperson71@gmail.com>
2019-02-01 06:27:39 -0500

    Added some recipes, more to come

--- src/main/java/abused_master/techutilities/registry/ModRecipes.java
+++ src/main/java/abused_master/techutilities/registry/ModRecipes.java
@@ -1,8 +1,16 @@
 package abused_master.techutilities.registry;
 
+import abused_master.techutilities.items.EnumResourceItems;
+import abused_master.techutilities.utils.RecipeGenerator;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+
 public class ModRecipes {
 
     public static void registerRecipes() {
-        //RecipeGenerator.createShaped(new ItemStack(ModBlocks.ENERGY_FURNACE), null, new RecipeGenerator.ShapedParser("GFG", ""));
+        RecipeGenerator.createShaped(new ItemStack(ModBlocks.ENERGY_FURNACE), null, new RecipeGenerator.ShapedParser("GFG", "PRP", "PCP", 'G', Blocks.GLASS, 'F', Blocks.FURNACE, 'P', EnumResourceItems.PHASE_INGOT.getItemIngot(), 'C', ModBlocks.PHASE_CELL));
+        RecipeGenerator.createShaped(new ItemStack(ModBlocks.PULVERIZER), null, new RecipeGenerator.ShapedParser("PRP", "PRP", "FCF", 'P', EnumResourceItems.PHASE_INGOT.getItemIngot(), 'R', Items.REDSTONE, 'F', Items.FLINT, 'C', ModBlocks.PHASE_CELL));
+        RecipeGenerator.createShaped(new ItemStack(ModBlocks.QUARRY), null, new RecipeGenerator.ShapedParser("ORO", "PDP", "OCO", 'O', Blocks.OBSIDIAN, 'R', Items.REDSTONE, 'P', Items.DIAMOND_PICKAXE, "C", ModBlocks.PHASE_CELL));
     }
 }
